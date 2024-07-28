68af9eadf7877f8e62f30e201cbeb710233b9bda
elijahescamilla1 <154039956+elijahescamilla1@users.noreply.github.com>
2024-07-28 06:18:09 -0700

    Updates for packages

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD9eWGVtB0M0H4jTWeh4jWPKM4eKDuAnzw",
   authDomain: "travel-itinerary-cdcf2.firebaseapp.com",
@@ -18,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
+
+export { auth };
