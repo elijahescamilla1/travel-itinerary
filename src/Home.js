9ad35ebbfdb3d4232efb93827adcb47e8a0a8ce1
Elijah Escamilla <elijah.escamilla@gmail.com>
2024-09-18 04:37:14 -0700

    Updates for login functions again

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token'); // Check if JWT token is stored in localStorage
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      {isLoggedIn ? (
+        <>
+          <h1>Welcome to the Home Page</h1>
+          {/* Add more content for logged-in users here */}
+        </>
+      ) : (
+        <>
+          <h1>Please Sign In</h1>
+          {/* You can add a redirect to the login page or display the login form directly here */}
+        </>
+      )}
     </div>
   );
 }
